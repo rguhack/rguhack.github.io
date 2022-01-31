adc76f4d221e2724df84318a9c550db04772b06a
daskoorb <daskoorb@gmail.com>
2022-01-31 23:10:45 +0000

    Add 2022 date

--- js/main.js
+++ js/main.js
@@ -48,7 +48,7 @@ function initializeClock(id, endtime){
 $(document).ready(function() {
   $(".button-collapse").sideNav();
 
-  var deadline = '2023-01-01T00:00:00Z';
+  var deadline = '2022-03-19T10:00:00Z';
   initializeClock('clock', deadline);
 
   // $('.info-section form').on('submit', function(e) {
